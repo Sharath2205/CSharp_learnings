eefa952e3e5fbf56069771488fd1c900ab460b75
Pendyala Sharath Chandra <sharath.chandra@mazikglobal.com>
2023-07-20 06:21:30 +0530

    Interfaces

---
+++ AbstractClasses/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+/*
+    Abstract Classes:
+    - Restricted class that cannot be used to create objects.
+    - Abstract method can only be used in an abstract class, and it does not have a body. The body is provided by the derived class.
+    - Contains abstract methods and regular methods.
+*/
+
+public abstract class Customer
+{
+    public void RegularPrint()  // Non abstract method.
+    {
+        Console.WriteLine("Printing regular method");
+    }
+
+    public abstract void AbstractPrint();
+}
+
+class CustomerClass : Customer 
+{
+    public override void AbstractPrint()
+    {
+        Console.WriteLine("Printing Abstract class method from derived class");
+    }
+}
+
+
+class Program
+{
+    static void Main()
+    {
+        CustomerClass cust = new CustomerClass();
+        cust.RegularPrint();
+        cust.AbstractPrint();
+    }
+}
--- Interfaces/Program.cs
+++ Interfaces/Program.cs
@@ -1,9 +1,66 @@
 ﻿using System;
 
-class Program
+/*
+    - Interface is a collection of abstract methods.
+    - Interface only have method declarations. Classes which inherit the interface must provide definations for all the methods declared in the interface.
+    - Interface only contains methods. Fields are not allowed in an interface
+    - Interface members are PUBLIC by default. We cannot explicitly have access modifiers for interface methods.
+*/
+
+interface ICustomer
 {
+    void Print();
+}
+
+class Customer : ICustomer
+{
+    public void Print() { }
+}
+
+
+// ------------- Explicit implementation
+
+interface I1
+{
+    void PrintInterface();
+}
+
+interface I2
+{
+    void PrintInterface();
+} 
+
+public class Program : I1, I2
+{
+
+    //public void PrintInterface()
+    //{
+    //    Console.WriteLine("Interface method");
+    //}
+
+
+    // Explict implementation
+    void I1.PrintInterface()
+    {
+        Console.WriteLine("Interface 1");
+    }
+
+    void I2.PrintInterface()
+    {
+        Console.WriteLine("Interface 2");
+    }
+
+
     static void Main()
     {
-        Console.WriteLine("Test");
+        //Program p1 = new Program();
+
+        //p1.PrintInterface();
+
+        I1 i1 = new Program();
+        i1.PrintInterface();
+
+        I2 i2 = new Program();
+        i2.PrintInterface();
     }
 }
